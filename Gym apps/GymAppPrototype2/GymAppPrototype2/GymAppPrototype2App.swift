2745363b3cd198fb24e6e8f9f8e6b2dc8484f189
Edwin-Trejo <60363120+Edwin-Trejo@users.noreply.github.com>
2022-06-14 13:21:33 -0600

    Initial commit prototype 2

---
+++ Gym apps/GymAppPrototype2/GymAppPrototype2/GymAppPrototype2App.swift
@@ -0,0 +1,17 @@
+//
+//  GymAppPrototype2App.swift
+//  GymAppPrototype2
+//
+//  Created by Edwin Trejo on 6/14/22.
+//
+
+import SwiftUI
+
+@main
+struct GymAppPrototype2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
